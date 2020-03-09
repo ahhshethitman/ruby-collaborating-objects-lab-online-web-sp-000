550358306e0608d64b51a3d066477809c2d7b3b7
Hunter Sides <hunter.s.1337@gmail.com>
2020-03-09 17:14:06 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class MP3Importer
+  attr_accessor :path, :files 
+  def initialize(path)
+    @path = path 
+
+    
+  end 
+  
+  def files
+    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+  end
+  
+end 
